4aae3e8ea52d9d641d7e7e9a8abac335b3541d5a
mil0237 <haemilkim7@gmail.com>
2025-12-08 17:04:04 +0900

    main menu sound, bossship color

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,15 +1,27 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class MenuController : MonoBehaviour
 {
 
     private StartButtonEffect startButtonEffect; //버튼 이펙트 오브젝트 참조
+    public float delayBeforeLoad = 0.5f;
     public void OnClickStart()
     {
         if (startButtonEffect != null)
             startButtonEffect.StopAndReset(); //버튼 클릭 시 멈춤
 
-        SceneManager.LoadScene("Player1"); // (임시) 나중에 FirstScene으로 수정
+
+        startButtonEffect?.PlayClickSound();
+
+        // 씬 전환 딜레이 코루틴 실행
+        StartCoroutine(LoadSceneWithDelay());
+
+        IEnumerator LoadSceneWithDelay()
+        {
+            yield return new WaitForSeconds(delayBeforeLoad);
+            SceneManager.LoadScene("Player1"); // 나중에 FirstScene으로 교체
+        }
     }
 }
